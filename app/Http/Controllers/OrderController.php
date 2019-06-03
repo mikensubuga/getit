ba8e6ee3493908bcb289b5ebd241ef34146b6710
King-Mike <nsubugamike021@gmail.com>
2019-06-03 10:07:00 +0300

    fixed my seeling bug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -42,64 +42,61 @@ class OrderController extends Controller
         $user = Auth::user();
 
         $data = [
-            'user_id'=>$user->id,
-            'jobProfile_id'=>$request->profile_id,
-            'delivered'=> $request->delivered,
-            'unit'=> $request->price,
-            'quantity'=>$request->qty,
-            'total'=>$request->qty*$request->price,
-          ];
-         
+            'user_id' => $user->id,
+            'jobProfile_id' => $request->profile_id,
+            'delivered' => $request->delivered,
+            'unit' => $request->price,
+            'quantity' => $request->qty,
+            'total' => $request->qty * $request->price,
+        ];
 
-          // Generate a random Reference
-          $reference = mt_rand();
-          //Total price
-          $total = $request->qty*$request->price;
 
-        $url = 'https://www.easypay.co.ug/api/'; 
-        $payload = array( 'username' => 'e4098ee9210a3602', 
-        'password' => 'b7ca0ca102e6286b', 
-        'action' => 'mmdeposit', 
-        'amount' => $request->qty*$request->price, 
-        'phone'=> $request->mmnumber, 
-        'currency'=>'UGX', 
-        'reference'=>$reference, 
-        'reason'=>'Testing MM DEPOSIT' 
-        ); 
-         
-        
+        // Generate a random Reference
+        $reference = mt_rand();
+        //Total price
+        $total = $request->qty * $request->price;
+
+        $url = 'https://www.easypay.co.ug/api/';
+        $payload = array(
+            'username' => 'e4098ee9210a3602',
+            'password' => 'b7ca0ca102e6286b',
+            'action' => 'mmdeposit',
+            'amount' => $request->qty * $request->price,
+            'phone' => $request->mmnumber,
+            'currency' => 'UGX',
+            'reference' => $reference,
+            'reason' => 'Testing MM DEPOSIT'
+        );
+
+
         //open connection 
-        $ch = curl_init(); 
-         
+        $ch = curl_init();
+
         //set the url, number of POST vars, POST data 
-        curl_setopt($ch,CURLOPT_URL, $url); 
-        curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($payload)); 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,30); 
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($ch, CURLOPT_TIMEOUT, 400); //timeout in seconds 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         //execute post 
-        $result = curl_exec($ch); 
-         
+        $result = curl_exec($ch);
+
         // if(curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200)
         //     echo "something went wrong";
         //close connection 
-        curl_close($ch); 
+        curl_close($ch);
 
         $decoded_result = json_decode($result, true);
 
-        if($decoded_result['success'] == 0){
-            return redirect()->back()->with('error','Error! User did not approve the mobile money request in time');
-        }
-        else{
+        if ($decoded_result['success'] == 0) {
+            return redirect()->back()->with('error', 'Error! User did not approve the mobile money request in time');
+        } else {
             $user = User::find($user->id);
             $user->orders()->create($data);
-            return redirect()->back()->with('success','Order has been created and '.$total.' has been received');
+            return redirect()->back()->with('success', 'Order has been created and ' . $total . ' has been received');
         }
-
-
-
     }
 
     /**
@@ -111,37 +108,39 @@ class OrderController extends Controller
     public function show($id)
     {
 
-        $orders = Order::where('user_id','=',$id)->latest()->get();
-       // dd($orders);
-        return view('front.myOrder',compact('orders','users'));
-       // return "Orders for ".$id;
+        $orders = Order::where('user_id', '=', $id)->latest()->get();
+        // dd($orders);
+        return view('front.myOrder', compact('orders', 'users'));
+        // return "Orders for ".$id;
     }
 
     public function showSelling($id)
     {
 
+
         $user = User::find($id);
-        $jobprofile = $user->jobprofile()->first()->id;
+        if ($user->jobprofile()->first()) {
 
-        
-    
-
-      $ordersd = Order::where('jobProfile_id', '=',$jobprofile)->latest()->get();
-       
-        return view('front.mySelling',compact('ordersd','users'));
+            $jobprofile = $user->jobprofile()->first()->id;
+            $ordersd = Order::where('jobProfile_id', '=', $jobprofile)->latest()->get();
+            return view('front.mySelling', compact('ordersd', 'users'));
+        } else {
+            return view('front.mySelling2');
+        }
     }
 
-    public function showFunds($id) {
-        
+    public function showFunds($id)
+    {
+
         $user = User::find($id);
-        $jobprofile = $user->jobprofile()->first()->id;
-
-        
-    
-
-      $ordersd = Order::where('jobProfile_id', '=',$jobprofile)->latest()->get();
-       //return $ordersd;
-        return view('front.myFunds',compact('ordersd','users'));
+        if ($user->jobprofile()->first()) {
+            $jobprofile = $user->jobprofile()->first()->id;
+            $ordersd = Order::where('jobProfile_id', '=', $jobprofile)->latest()->get();
+            //return $ordersd;
+            return view('front.myFunds', compact('ordersd', 'users'));
+        } else {
+            return view('front.myFunds2');
+        }
     }
     /**
      * Show the form for editing the specified resource.
@@ -154,62 +153,62 @@ class OrderController extends Controller
         //
     }
 
-    public function withdraw(Request $request){
-      // return $request;
-         // Generate a random Reference
+    public function withdraw(Request $request)
+    {
+        // return $request;
+        // Generate a random Reference
         // $reference = mt_rand();
-        
-         //get amount from form
-         $total = $request->amount;
 
-       $url = 'https://www.easypay.co.ug/api/'; 
-       $payload = array( 'username' => 'e4098ee9210a3602', 
-       'password' => 'b7ca0ca102e6286b', 
-       'action' => 'mmpayout', 
-       'amount' => $request->amount, 
-       'phone'=> $request->mmnumber, 
-       'currency'=>'UGX'
-      // 'reference'=>$reference, 
-      // 'reason'=>'Withdraw' 
-       ); 
-        
-       
-       //open connection 
-       $ch = curl_init(); 
-        
-       //set the url, number of POST vars, POST data 
-       curl_setopt($ch,CURLOPT_URL, $url); 
-       curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($payload)); 
-       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
-       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
-       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,30); 
-       curl_setopt($ch, CURLOPT_TIMEOUT, 400); //timeout in seconds 
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-       //execute post 
-       $result = curl_exec($ch); 
-        
-       // if(curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200)
-       //     echo "something went wrong";
-       //close connection 
-       curl_close($ch); 
+        //get amount from form
+        $total = $request->amount;
 
-       $decoded_result = json_decode($result, true);
+        $url = 'https://www.easypay.co.ug/api/';
+        $payload = array(
+            'username' => 'e4098ee9210a3602',
+            'password' => 'b7ca0ca102e6286b',
+            'action' => 'mmpayout',
+            'amount' => $request->amount,
+            'phone' => $request->mmnumber,
+            'currency' => 'UGX'
+            // 'reference'=>$reference, 
+            // 'reason'=>'Withdraw' 
+        );
 
-       if($decoded_result['success'] == 0){
-           return redirect()->back()->with('error','An error Occured! Insufficient balance in the account');
-       }
-       else{
-        $user = Auth::user();
-        $jobprofile = $user->jobprofile()->first()->id;
-        $ord = Order::where('jobProfile_id', '=',$jobprofile)->latest()->get();
-        foreach($ord as $or){
-            if($or->delivered == 1)
-            $or->delete();
-            echo $or;
+
+        //open connection 
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data 
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 400); //timeout in seconds 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //execute post 
+        $result = curl_exec($ch);
+
+        // if(curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200)
+        //     echo "something went wrong";
+        //close connection 
+        curl_close($ch);
+
+        $decoded_result = json_decode($result, true);
+
+        if ($decoded_result['success'] == 0) {
+            return redirect()->back()->with('error', 'An error Occured! Insufficient balance in the account');
+        } else {
+            $user = Auth::user();
+            $jobprofile = $user->jobprofile()->first()->id;
+            $ord = Order::where('jobProfile_id', '=', $jobprofile)->latest()->get();
+            foreach ($ord as $or) {
+                if ($or->delivered == 1)
+                    $or->delete();
+                echo $or;
+            }
+            return redirect()->back()->with('success', 'An amount of ' . $total . ' has been Withdrawn from account');
         }
-           return redirect()->back()->with('success','An amount of '.$total.' has been Withdrawn from account');
-       }
-
     }
     /**
      * Update the specified resource in storage.
---
+++ resources/views/front/myFunds2.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.base')
+
+@section('content')
+
+<div class="card-header">
+    <h3> My Funds </h3>
+</div>
+    <!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">View Your Funds</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <p>You need to create a Job Profile first to access this page</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+    
+@endsection
